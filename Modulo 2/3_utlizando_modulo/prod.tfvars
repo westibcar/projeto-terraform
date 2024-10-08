6906b940df7469c3a69fec358277a6593125824c
Wesley Tibucio <wesley.tibucio@gmail.com>
2024-10-07 21:07:55 -0300

    add modules

---
+++ Modulo 2/3_utlizando_modulo/prod.tfvars
@@ -0,0 +1,3 @@
+env = "prod"
+vpc_cidr = "172.16.0.0/16"
+subnet_cidr =  "172.16.1.0/24"
--- Modulo 2/3_utlizando_modulo/variables.tf
+++ Modulo 2/3_utlizando_modulo/variables.tf
@@ -6,17 +6,8 @@ variable "account" {
   default = "udayaccount"
 }
 
-variable "env" {
-  default = "prod"
-}
+variable "env" {}
 
-variable "vpc_cidr" {
-  default = "10.0.0.0/16"
-}
-
-variable "subnet_cidr" {
-  default = "10.0.1.0/24"
-}
 
 variable "config" {
   description = "Configuração de cada ambiente"
@@ -40,4 +31,12 @@ variable "config" {
       key_pair      = "chave-ssh-prod"
     }
   }
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  default = "10.0.1.0/24"
 }
